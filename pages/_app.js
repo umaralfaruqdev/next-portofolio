276df3182e757755434c80fc73cf9af343622bd3
umaralfaruqdev <ualfaruq59@gmail.com>
2020-06-29 13:17:13 +0700

    fix error on build

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
+import SiteLayout from '@lay/SiteLayout';
+
 const MyApp = ({ Component, pageProps  }) => {
 	console.log(Component);
-	const getLayout = Component.getLayout;
+	const getLayout = Component.getLayout || (page => <SiteLayout children={page} />);
 
 	return getLayout(<Component {...pageProps}/>);
 }
